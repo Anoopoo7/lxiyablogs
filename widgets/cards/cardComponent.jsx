bbdf0bead6cb64fb5d7f81a49804d89e47c9f7f4
anoop007 <anoopviswanadh007@gmail.com>
2022-10-01 18:59:24 +0530

    card changes

--- widgets/cards/cardComponent.jsx
+++ widgets/cards/cardComponent.jsx
@@ -3,27 +3,31 @@
 const CardComponent = ({ cardData }) => {
   return (
     <div className="item-card m-2">
-      <a href="/product/abc" className="text-dark">
-        <div className="imgBox">
-          <img
-            src={cardData[0]?.images[0]}
-            alt="mouse corsair"
-            className="mouse"
-          />
-        </div>
-      </a>
-      <div className="contentBox text-decoration-none">
-        <h3>
-          {cardData[0]?.name.substring(0, 30)}
-          {cardData[0]?.name.length > 25 ? "..." : ""}
-        </h3>
-        <h2 className="price">
-        {cardData[1]?.defaultPrice}.<small>00</small> €
-        </h2>
-        <a href="#" className="buy">
-          Buy Now
-        </a>
-      </div>
+      {cardData && Array.isArray(cardData) && cardData.length == 2 && (
+        <>
+          <a href="/product/abc" className="text-dark">
+            <div className="imgBox">
+              <img
+                src={cardData[0]?.images[0]}
+                alt="mouse corsair"
+                className="mouse"
+              />
+            </div>
+          </a>
+          <div className="contentBox text-decoration-none">
+            <h3>
+              {cardData[0]?.name.substring(0, 30)}
+              {cardData[0]?.name.length > 25 ? "..." : ""}
+            </h3>
+            <h2 className="price">
+              {cardData[1]?.defaultPrice}.<small>00</small> €
+            </h2>
+            <a href="#" className="buy">
+              Buy Now
+            </a>
+          </div>
+        </>
+      )}
     </div>
   );
 };
