0dc9ac459c625606d29ac383661fedf404c1de06
anoop007 <anoopviswanadh007@gmail.com>
2022-12-06 23:12:54 +0530

    backend url changes

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   // backendUrl:"http://localhost:8080"
-  backendUrl:"https://ecom-uat-app.herokuapp.com"
+  backendUrl:"https://ecom-blog-uat.up.railway.app"
 };
 
 module.exports = nextConfig;
